ae369164b1e873c580c6f660557dc51ad26a797f
Thiago Campos <=>
2023-11-23 03:21:43 -0300

    create component exibe-favoritos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AdicionaContatoComponent } from './adiciona-contato/adiciona-contato.component';
 import { ExibeContatosComponent } from './exibe-contatos/exibe-contatos.component';
+import { ExibeFavoritosComponent } from './exibe-favoritos/exibe-favoritos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdicionaContatoComponent,
-    ExibeContatosComponent
+    ExibeContatosComponent,
+    ExibeFavoritosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/exibe-favoritos/exibe-favoritos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exibe-favoritos',
+  templateUrl: './exibe-favoritos.component.html',
+  styleUrls: ['./exibe-favoritos.component.css']
+})
+export class ExibeFavoritosComponent {
+
+}
